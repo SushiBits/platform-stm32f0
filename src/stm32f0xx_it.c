653d695685327f62f068276590016ea9d231036c
Max Chan <xcvista@me.com>
2017-11-08 02:05:16 +0800

    Introducing linder scripts

--- src/stm32f0xx_it.c
+++ src/stm32f0xx_it.c
@@ -8,6 +8,7 @@
 #include <stm32f0xx_it.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <string.h>
 
 void __stack(void);
 void Reset_IRQHandler(void);
@@ -23,21 +24,64 @@ void (*ISR_Vector[])(void) =
 #undef SKIP_HANDLER
 };
 
+extern const struct copyitem
+{
+	const void *src;
+	void *dst;
+	uint32_t size;
+} *__copy_addr;
+extern uint32_t __copy_size;
+
+extern const struct zeroitem
+{
+	void *dst;
+	uint32_t size;
+} *__zero_addr;
+extern uint32_t __zero_size;
+
 __attribute__((noreturn)) void __start(void);
 
 __attribute__((noreturn)) void Reset_IRQHandler(void)
 {
+	uint32_t count = __copy_size / sizeof(struct copyitem);
+	for (uint32_t idx = 0; idx < count; idx++)
+	{
+		const struct copyitem *item = &(__copy_addr[idx]);
+		memcpy(item->dst, item->src, item->size);
+	}
+
+	count = __zero_size / sizeof(struct zeroitem);
+	for (uint32_t idx = 0; idx < count; idx++)
+	{
+		const struct zeroitem *item = &(__zero_addr[idx]);
+		memset(item->dst, 0, item->size);
+	}
+
 	for (;;);
 }
+
+#define DHCSR (*((volatile uint32_t *)0xe000edf0))
 
 __attribute__((noreturn)) void Default_IRQHandler(void)
 {
-	for (;;);
+	if (DHCSR & 1)
+		__BKPT(0);
+
+	for (;;)
+		__WFE();
 }
 
-__attribute__((noreturn)) void _exit(int value)
+__attribute__((noreturn)) void _exit(int status)
 {
-	for (;;);
+	if (!status && (DHCSR & 1))
+		__BKPT(0);
+
+	SCB->AIRCR =
+			(0x05fa << SCB_AIRCR_VECTKEY_Pos) |
+			SCB_AIRCR_SYSRESETREQ_Msk;
+
+	for (;;)
+		__WFE();
 }
 
 #define IRQN_HANDLER(id, name) __attribute__((weak, alias("Defualt_IRQHandler"))) void name ##_IRQHandler(void);
